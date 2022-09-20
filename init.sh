abb3a02d7c513dca0b8043e5fcb69bd3504f61ea
uratch <kenji.uraoka@gmail.com>
2022-09-20 03:16:18 +0000

    save

--- init.sh
+++ init.sh
@@ -10,3 +10,13 @@ fi
 
 mkdir -p dir_nas/dir
 mkdir -p dir_anf/dir
+
+echo "A" > dir_nas/a.txt
+echo "A" > dir_nas/dir/a.txt
+echo "A" > dir_anf/a.txt
+echo "A" > dir_anf/dir/a.txt
+
+echo "B" > dir_nas/b.txt
+echo "B" > dir_nas/dir/b.txt
+echo "b" > dir_anf/b.txt
+echo "b" > dir_anf/dir/b.txt
