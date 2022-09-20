4914b679dc92545d2f62e85b17fc305ac002b818
uratch <kenji.uraoka@gmail.com>
2022-09-20 08:48:18 +0000

    save

--- init.sh
+++ init.sh
@@ -8,56 +8,58 @@ if [ -d dir_anf ];then
   rm -rf dir_anf
 fi
 
-mkdir -p dir_nas/dir
-mkdir -p dir_anf/dir
+mkdir dir_nas
+mkdir dir_anf
+###mkdir dir_nas/dir
+###mkdir dir_anf/dir
 
 #
 # both same file.
 #
 echo "A" > dir_nas/a.txt
-echo "A" > dir_nas/dir/a.txt
+###echo "A" > dir_nas/dir/a.txt
 # add sleep difference for buffering.
 sleep 1
 echo "A" > dir_anf/a.txt
-echo "A" > dir_anf/dir/a.txt
+###echo "A" > dir_anf/dir/a.txt
 
 touch -r dir_nas/a.txt     dir_anf/a.txt
-touch -r dir_nas/dir/a.txt dir_anf/dir/a.txt
+###touch -r dir_nas/dir/a.txt dir_anf/dir/a.txt
 
 #
 # different file.
 #
 echo "B" > dir_nas/b.txt
-echo "B" > dir_nas/dir/b.txt
+###echo "B" > dir_nas/dir/b.txt
 # add sleep difference for buffering.
-sleep 1
+sleep 2
 echo "b" > dir_anf/b.txt
-echo "b" > dir_anf/dir/b.txt
+###echo "b" > dir_anf/dir/b.txt
 
 #
 # milli sec difference file.
 #
 echo "C" > dir_nas/c.txt
-echo "C" > dir_nas/dir/c.txt
+###echo "C" > dir_nas/dir/c.txt
 echo "C" > dir_anf/c.txt
-echo "C" > dir_anf/dir/c.txt
+###echo "C" > dir_anf/dir/c.txt
 
 TIMESTAMP=`stat dir_nas/c.txt |grep Modify|awk '{print $2$3}'| sed -e 's/[-|:]//g' | awk -F'.' '{print $1}' | sed -e 's/\(..\)$/.\1/'`
 touch -t $TIMESTAMP dir_anf/c.txt
-TIMESTAMP=`stat dir_nas/dir/c.txt |grep Modify|awk '{print $2$3}'| sed -e 's/[-|:]//g' | awk -F'.' '{print $1}' | sed -e 's/\(..\)$/.\1/'`
-touch -t $TIMESTAMP dir_anf/dir/c.txt
+###TIMESTAMP=`stat dir_nas/dir/c.txt |grep Modify|awk '{print $2$3}'| sed -e 's/[-|:]//g' | awk -F'.' '{print $1}' | sed -e 's/\(..\)$/.\1/'`
+###touch -t $TIMESTAMP dir_anf/dir/c.txt
 
 #
 # only NAS.
 #
 echo "D" > dir_nas/d.txt
-echo "D" > dir_nas/dir/d.txt
+###echo "D" > dir_nas/dir/d.txt
 
 #
 # only ANF.
 #
 echo "E" > dir_anf/e.txt
-echo "E" > dir_anf/dir/e.txt
+###echo "E" > dir_anf/dir/e.txt
 
 #
 # find
